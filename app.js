64f2e7801c2cd40022c1f66ede01417503d92618
CellOrwell <joshuamilroy@hotmail.co.uk>
2023-08-11 01:02:14 +0100

    Add DB Connection

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const mysql = require("mysql");
+const os = require("os");
 require("dotenv").config();
 
-const app = express();
+console.log(os.networkInterfaces());
+// const localIP = os.networkInterfaces()['eth0'][0].address;
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+//    password: "Bzh7p6r5LJZ*F#V",
+
+
+
+var con = mysql.createConnection({
+    host: "192.168.1.85",
+    port: 3306,
+    user: `toDoUser`,
+    password: "Bzh7p6r5LJZ",
+    database: "toDoDB"
+});
+
+con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected.");
+});
